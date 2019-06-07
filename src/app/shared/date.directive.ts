7e3ac67fca9435e5a165543768ddf2735d5c11af
Katarzyna <kaskafaras@gmail.com>
2019-06-07 12:31:31 +0200

    refactoring

--- src/app/shared/date.directive.ts
+++ src/app/shared/date.directive.ts
@@ -13,18 +13,24 @@ export class DateDirective {
   @Input()
   private date: string;
   private paragraph;
+  private hasChild;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
     this.paragraph = this.renderer.createElement("span");
+    this.hasChild = false;
   }
 
-  @HostListener("mouseenter")
-  mouseenter(eventDate: Event) {
-    this.paragraph.innerHTML = " Date:  " + this.date;
-    this.renderer.appendChild(this.el.nativeElement, this.paragraph);
-  }
-  @HostListener("mouseleave")
-  mouseleave(eventDate: Event) {
-    this.renderer.removeChild(this.el.nativeElement, this.paragraph);
+  @HostListener("click")
+  click(eventDate: Event) {
+    setTimeout(() => {
+      if (!this.hasChild) {
+        this.paragraph.innerHTML = " Date:  " + this.date;
+        this.renderer.appendChild(this.el.nativeElement, this.paragraph);
+        this.hasChild = true;
+      } else {
+        this.renderer.removeChild(this.el.nativeElement, this.paragraph);
+        this.hasChild = false;
+      }
+    }, 100);
   }
 }
